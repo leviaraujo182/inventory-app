06940dadbbb07701db2c630041011c5733098a45
leviaraujo182 <31797494+leviaraujo182@users.noreply.github.com>
2021-05-16 12:44:35 -0300

    Firebase config auth

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Card, TextInput, Button } from 'react-native-paper'
+import firebase from '../../firebaseConfig'
 
 
 export default function Login({navigation}) {
+
+
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
 
     const theme = {
         colors: {
@@ -22,15 +27,33 @@ export default function Login({navigation}) {
         navigation.navigate('Home')
     }
 
+    function FirebaseLogin(){
+        firebase.auth().signInWithEmailAndPassword(email, senha).then(()=>{
+            navigation.navigate("Home");
+        }).catch(()=>{
+            console.log("Não logou");
+        })
+    }
+
+    useEffect(()=>{
+        firebase.auth().onAuthStateChanged(function(user){
+            if(user){
+                console.log("Logado com sucesso", user.uid);
+            } else {
+                console.log("Não logado")
+            }
+        })
+    }, [])
+
  return (
    <View style={styles.body}>
        <Text style={styles.title}>INVENTORY</Text>
        <Text style={styles.subtitle}>O SEU INVENTÁRIO DIGITAL</Text>
        <Card style={styles.card}>
            <Text style={styles.txtlogin}>LOGIN</Text>
-           <TextInput label="Email" style={styles.inpt_email} theme={theme} />
-           <TextInput label="Senha" style={styles.inpt_senha} theme={theme} />
-           <Button mode='contained' style={styles.btn_entrar} onPress={ScreenHome}>Entrar</Button>
+           <TextInput label="Email" style={styles.inpt_email} theme={theme} onChangeText={email => setEmail(email)} value={email} />
+           <TextInput label="Senha" style={styles.inpt_senha} theme={theme} onChangeText={senha => setSenha(senha)} value={senha} secureTextEntry={true} />
+           <Button mode='contained' style={styles.btn_entrar} onPress={FirebaseLogin}>Entrar</Button>
            <Button mode='outlined' style={styles.btn_cadastre} theme={btncad}>Cadastre-se</Button>
            <TouchableOpacity>
                <Text style={styles.txtproblem}>Problemas com login?</Text>
@@ -65,7 +88,7 @@ const styles = StyleSheet.create({
     card: {
         marginTop: 20,
         width: '90%',
-        height: '50%',
+        height: 400,
         borderRadius: 15
     },
 
