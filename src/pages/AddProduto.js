e53873a5766f0673f90c2fa9f732c004109765ab
leviaraujo182 <31797494+leviaraujo182@users.noreply.github.com>
2021-05-23 18:29:32 -0300

    AddProduto update

--- src/pages/AddProduto.js
+++ src/pages/AddProduto.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { View,Text, StyleSheet, Picker} from 'react-native';
+import React, { useState } from 'react';
+import { View,Text, StyleSheet} from 'react-native';
 import { Card, TextInput } from 'react-native-paper'
+import { Picker } from '@react-native-picker/picker'
 
 
 export default function pages() {
@@ -11,6 +12,8 @@ export default function pages() {
     }
   }
 
+  const [selectedLanguage, setSelectedLanguage] = useState();
+
  return (
    <View style={styles.body}>
        <Card style={styles.card}>
@@ -18,7 +21,15 @@ export default function pages() {
             <Text style={styles.txt_preencha}>Preencha todos os campos corretamente</Text>
             <TextInput label="Nome do produto" style={styles.input_addprod} theme={inpt_style} />
             <TextInput label="Marca do produto" style={styles.input_addprod} theme={inpt_style} />
-          
+            <View style={styles.est_picker}>
+            <Picker style={styles.picker} selectedValue={selectedLanguage} onValueChange={(itemValue, itemIndex) => setSelectedLanguage(itemValue)}>
+                <Picker.Item label="Alimentos" value="java" />
+                <Picker.Item label="Beleza" value="js" />
+                <Picker.Item label="Higiene" value="java" />
+                <Picker.Item label="Limpeza" value="js" />
+            </Picker>
+            </View>
+            <TextInput keyboardType='phone-pad' label="Quantidade" style={styles.input_addprod} theme={inpt_style} />
           </View>
        </Card>
    </View>
@@ -58,5 +69,20 @@ const styles = StyleSheet.create({
       width: '95%',
       height: 60,
       backgroundColor: '#FFF',
+    },
+
+    picker: {
+      width: '100%',
+      height: 50,
+    },
+
+    est_picker: {
+      marginTop: 10,
+      width: '95%',
+      borderWidth: 1,
+      borderColor: '#d9d9d9',
+      marginRight: 10,
+      marginLeft: 10,
+      borderRadius: 10
     }
 })
